44211e0c3f8b42b435fbbe13bdd677022e90ac03
Alex Gearing <alexmgearing@gmail.com>
2020-10-26 23:28:35 +1100

    :wrench: :shirt: Add custom linting settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,34 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/recommended',
+    '@vue/standard',
+    'plugin:jest/recommended',
+    'plugin:cypress/recommended',
+  ],
+  plugins: [
+    'vue',
+    'jest',
+    'cypress'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Allow async-await.
+    'generator-star-spacing': 'off',
+    // Allow debugger during development.
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // Enforce style of a single space before curly bracket open & close.
+    'object-curly-spacing': ['error', 'always'],
+    // Enforce style of no spaces before array open & close.
+    'array-bracket-spacing': ['error', 'never'],
+    // Enforce style of seperate lines over blocks for multiline comments.
+    'multiline-comment-style': ['error', 'separate-lines'],
+    // Require comma-dangle in multi-line arrays and objects.
+    'comma-dangle': ['error', 'always-multiline']
   },
   overrides: [
     {
