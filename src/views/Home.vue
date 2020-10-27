3e0dcdcd6b1e358ad6ac37f928ce4ed260bd7bd5
Alex Gearing <alexmgearing@gmail.com>
2020-10-27 14:33:29 +1100

    :balloon: Add new AnimatedLogo component with line art animations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <animated-logo />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import AnimatedLogo from '@/components/AnimatedLogo.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    AnimatedLogo,
+  },
 }
 </script>
