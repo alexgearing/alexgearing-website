7cafc2434c81178d56882f3c76e6414ef08eccbf
Alex Gearing <alexmgearing@gmail.com>
2020-11-09 23:06:06 +1100

    :wrench: :package: Add vue-i18n plugin

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,13 @@ module.exports = {
       },
     },
   },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
 }
