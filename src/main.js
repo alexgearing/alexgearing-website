9546f69a0313950ba0229325e562ffa0f85cdf0e
Alex Gearing <alexmgearing@gmail.com>
2020-12-10 23:36:47 +1100

    :wrench: Add utility to automatically register global components

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import registerGlobalComponents from '@/utils/registerGlobalComponents'
 import '@/assets/scss/index.scss'
 import i18n from './i18n'
 
 Vue.config.productionTip = false
+
+registerGlobalComponents()
 
 new Vue({
   router,
