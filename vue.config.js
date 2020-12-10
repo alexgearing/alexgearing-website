dfa214ca2e6e7bde640f10ac89b3cd4df65d0857
Alex Gearing <alexmgearing@gmail.com>
2020-12-10 23:36:19 +1100

    :wrench: :package: Add vue-svg-loader package

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,35 @@ module.exports = {
       locale: 'en',
       fallbackLocale: 'en',
       localeDir: 'locales',
-      enableInSFC: true
-    }
-  }
+      enableInSFC: true,
+    },
+  },
+
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+
+    svgRule.uses.clear()
+
+    svgRule
+      .oneOf('inline')
+      .resourceQuery(/inline/)
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [
+            { cleanupIDs: false },
+            { collapseGroups: false },
+          ],
+        },
+      })
+      .end()
+      .end()
+      .oneOf('external')
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]',
+      })
+  },
 }
