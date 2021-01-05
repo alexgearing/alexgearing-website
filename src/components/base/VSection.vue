99b7dd72dd550698c9423f40926b4f2c6bf8968d
Alex Gearing <alexmgearing@gmail.com>
2021-01-05 16:45:34 +1100

    :art: :iphone: Add responsibity to VSection

--- src/components/base/VSection.vue
+++ src/components/base/VSection.vue
@@ -1,11 +1,23 @@
 <template>
   <section class="v-section">
-    <div class="v-section__content">
-      <div class="v-section__container-text">
+    <div
+      :class="[
+        'v-section__content',
+        { 'v-section__content--is-left': !hasImageOnLeft }
+      ]"
+    >
+      <div
+        :class="[
+          'v-section__container',
+          'v-section__container-text',
+          `v-section__container-text--width--${textWidth}`,
+        ]"
+      >
         <slot name="text" />
       </div>
       <div
         :class="[
+          'v-section__container',
           'v-section__container-image',
           { 'v-section__container-image--is-left': hasImageOnLeft }
         ]"
@@ -24,6 +36,7 @@ export default {
   name: 'VSection',
   props: {
     hasImageOnLeft: { type: Boolean, default: false },
+    textWidth: { type: Number, default: 60 },
   },
 }
 </script>
@@ -32,9 +45,11 @@ export default {
 
 .v-section {
   position: relative;
+  display: flex;
+  justify-content: center;
   padding: 8rem 4rem;
 
-  @include media('<tablet') {
+  @include media('<desktop') {
     padding: 4rem 2rem;
   }
 
@@ -49,24 +64,51 @@ export default {
     max-width: 1280px;
     padding: var(--rem--xl);
     margin: auto;
+    display: flex;
+    flex-wrap: nowrap;
+    margin: 0;
+    padding: 0;
 
+    @include media('<desktop') {
+      flex-direction: column;
+
+      &--is-left {
+        flex-direction: column-reverse;
+      }
+    }
   }
 
   &__container {
+    box-sizing: border-box;
+    padding: var(--rem--xl);
+    transition: all 1s ease;
+    flex: 1;
+
     &-text {
-      order: 2
+      order: 2;
+
+      @each $width, $percentage in $percentage-map {
+        &[class*="width--#{$width}"] {
+          @include media('>=desktop') {
+            flex: 0 0 $percentage; max-width: $percentage;
+          }
+        }
+      }
     }
 
     &-image {
       min-width: 0;
       min-height: 0;
-      width: 100%;
-      height: 100%;
-      max-width: 382px;
+      min-width: 300px;
       order: 3;
 
+      @include media('<desktop') {
+        width: 100%;
+        max-width: 420px;
+      }
+
       &--is-left {
-        order: 1
+        order: 1;
       }
     }
   }
